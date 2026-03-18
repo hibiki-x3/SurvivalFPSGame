d217ff3dd492900463035b98c32479647a13a22d
hibiki <bruhmoment2206@gmail.com>
2026-03-18 19:27:11 +0700

    Revert "Added bullet holes"

--- My project/Assets/Scripts/Bullet.cs
+++ My project/Assets/Scripts/Bullet.cs
@@ -4,39 +4,18 @@ using System.Collections.Generic;
 
 public class Bullet : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision objectWeHit)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (objectWeHit.gameObject.CompareTag("Target"))
+        if (collision.gameObject.CompareTag("Target"))
         {
-            print("hit " + objectWeHit.gameObject.name + " !");
-            CreateBulletImpactEffect(objectWeHit);
+            print("hit " + collision.gameObject.name + " !");
             Destroy(gameObject);
         }
 
-        if (objectWeHit.gameObject.CompareTag("Wall"))
+        if (collision.gameObject.CompareTag("Wall"))
         {
             print("hit a wall");
-            CreateBulletImpactEffect(objectWeHit);
             Destroy(gameObject);
         }
     }
-
-    void CreateBulletImpactEffect(Collision objectWeHit)
-    {
-        ContactPoint contact = objectWeHit.contacts[0];
-
-        GameObject hole = Instantiate(
-            GlobalReferences.Instance.bulletImpactEffectPrefab,
-            contact.point,
-            Quaternion.LookRotation(contact.normal)
-        );
-
-        hole.transform.SetParent(objectWeHit.gameObject.transform);
-
-
-
-    }
-
-
-
 }
