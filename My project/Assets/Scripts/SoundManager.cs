8bc5250a3005ea81658e44bab3812fbce40f53c5
hibiki-x3 <bruhmoment2206@gmail.com>
2026-03-19 17:03:08 +0700

    added a pistol and temporary disabled bullet holes effect when hitting wall
    pistol have idle and shooting animation, muzzle flash and shooting sound

---
+++ My project/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance{ get; set; }
+
+    public AudioSource shootingSoundM1911;
+
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+}
