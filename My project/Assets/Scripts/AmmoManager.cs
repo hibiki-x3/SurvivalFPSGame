03409a6a02d4018bff206169cd07ceee165a653c
hibiki-x3 <bruhmoment2206@gmail.com>
2026-03-19 17:50:05 +0700

    added an ammo system and added more pistol sounds

--- My project/Assets/Scripts/AmmoManager.cs
+++ My project/Assets/Scripts/AmmoManager.cs
@@ -1,14 +1,14 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
 
-public class SoundManager : MonoBehaviour
+public class AmmoManager : MonoBehaviour
 {
-    public static SoundManager Instance{ get; set; }
+    public static AmmoManager Instance{ get; set; }
 
-    public AudioSource shootingSoundM1911;
-    public AudioSource reloadingSoundM1911;
-    public AudioSource emptyMagazineSoundM1911;
+    //UI
+    public TextMeshProUGUI ammoDisplay;
 
     private void Awake()
     {
